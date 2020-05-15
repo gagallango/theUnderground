856e463c98dbb6bbf7216d824936cc333ecfb203
Gabriela Gallango <ga.gallango@gmail.com>
2020-05-15 12:15:10 +0200

    hello

--- server/configs/views.configs.js
+++ server/configs/views.configs.js
@@ -1,3 +1,4 @@
+const favicon = require('serve-favicon')
 const path = require('path')
 const express = require('express')
 
@@ -5,4 +6,5 @@ module.exports = app => {
     app.set('views', path.join(__dirname, '..', 'views'))
     app.set('view engine', 'hbs')
     app.use(express.static(path.join(__dirname, '..', 'public')))
+    app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
 }
