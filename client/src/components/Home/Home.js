47415330075a0e9e53fcc97683073f9569e9eedf
Gabriela Gallango <ga.gallango@gmail.com>
2020-07-02 13:15:54 +0200

    add ola

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             </div>
             <section>
                 <div className="scroll text">
-                    <div>a writing and content creator platform - a writing and content creator platform - a writing and content creator plat</div>
+                    <div>holaaaaaa a writing and content creator platform - a writing and content creator platform - a writing and content creator plat</div>
                 </div>
             </section>
         </>
